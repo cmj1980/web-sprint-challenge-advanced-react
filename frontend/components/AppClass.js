5c50cee070e2f1d41f5244fb41f9f1980f40b8a0
cmj1980 <dagoclmt@gmail.com>
2022-05-13 16:34:44 -0700

    grid and coordinates working

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,24 +1,50 @@
 import React from 'react'
 
 export default class AppClass extends React.Component {
+
+  state = {
+    steps: 0,
+    grid: [null, null, null, null, "B", null, null, null, null], 
+    email: "",
+    message: "",
+  }
+
+  getPositionOfXY = () => {
+    const c = this.state.grid.indexOf("B");
+    let r;
+    return c < 3 ? r = 1 : c < 6 ? r = 2 : c < 9 && (r = 3),
+    [c % 3 + 1, r]
+  };
+
+  getMessageOfXY = () => {
+    const [c, r] = this.getPositionOfXY();
+    return `Coordinates (${c}, ${r})`
+  };
+
+  reset = () => {
+    this.setState()
+  };
+
+  
+
+
+
+
+
+
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{this.getMessageOfXY()}</h3>
+          <h3 id="steps">{`You moved ${this.state.steps} times`}</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          {this.state.grid.map((gridLetter, idx) => (
+            <div key={idx} className="square">{gridLetter}
+          </div>))};
         </div>
         <div className="info">
           <h3 id="message"></h3>
